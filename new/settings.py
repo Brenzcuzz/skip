36929085205d50a344ac9621b70dc519278e3882
den <holdenn119@gmail.com>
2023-09-13 20:43:17 +0100

    setter

--- new/settings.py
+++ new/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / 'staticfiles_build'/ "static"
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
