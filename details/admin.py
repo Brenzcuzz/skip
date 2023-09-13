02a38e2a18dcfd73725003bcab64f0e95b71f422
den <holdenn119@gmail.com>
2023-09-13 18:19:50 +0100

    add model

--- details/admin.py
+++ details/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Mymodel
 
 # Register your models here.
+class LoginAdmin(admin.ModelAdmin):
+    list_display = ('username','password')
+
+# Register your models here.
+admin.site.register(Mymodel,LoginAdmin)
--- new/settings.py
+++ new/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7um(b%rmtnhdxa$t(w%$@4=s=(fw!v#@+paav%*!zbx$6n(lbq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -127,14 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles_build'/ "static"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 ]
 
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
